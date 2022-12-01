c717c1c177607ba0d8d1de5d0f9104c878c50c1f
allawhussein <allawhussein@gmail.com>
2022-12-01 07:32:14 +0200

    solved os ass 1 bug

--- i3303_operating_systems_2/assignments/assignment_1.c
+++ i3303_operating_systems_2/assignments/assignment_1.c
@@ -9,17 +9,24 @@
 
 int main(){
         int a = 0, n = 0, temp = 0;
+        printf("This program get the maximum number by dividing the search task between two process\n");
+        printf("parent PID: %d\n", getpid());
+        printf("enter the size of the input: ");
         scanf("%d", &n);
         int *arr = (int *)malloc(n * sizeof(int));
         for (int i = 0; i < n; i++) scanf("%d", arr + i);
-        if (fork()) n = n/2;
+        if (fork()) n = n/2 + 1;
         else a = n/2 + 1;
         temp = *arr;
-        for (int i = a; i < n; i++)
-                if (temp > *(arr + i)) temp = *(arr + i);
+        for (int i = a; i < n; i++){
+                if (temp < *(arr + i)) temp = *(arr + i);
+                printf("process %d processed number %d\n", getpid(), *(arr+i));
+        }
         if (a != 0) exit(temp);
         wait(&a);
-        if (a > temp) temp = a;
+        a = a >> 8;
+        printf("forked process biggest number is %d\n", a);
+        if (temp < a) temp = a;
         printf("The biggest value is %d", temp);
         return 0;
 }
