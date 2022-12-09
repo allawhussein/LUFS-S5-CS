04d83527877e6b3510db68aa0152f371520ea2f3
allawhussein <allawhussein@gmail.com>
2022-12-09 08:34:01 +0200

    fixed uresolved links error in numberguess android project

--- i3350_mobile_development/Apps/NumberGuess/app/src/main/java/com/example/numberguess/MainActivity.java
+++ i3350_mobile_development/Apps/NumberGuess/app/src/main/java/com/example/numberguess/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.numberguess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView guessTextView;
+    TextView guessCtr;
+    Button submitButton;
+    Button rstBtn;
+    EditText guessInput;
+    int randInt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        guessTextView = findViewById(R.id.guess_text);
+        submitButton = findViewById(R.id.entr_btn);
+        rstBtn = findViewById(R.id.rst_btn);
+        guessCtr = findViewById(R.id.guess_ctr);
+        guessCtr.setText("0");
+        guessInput = findViewById(R.id.guess_field);
+        randInt = (int)(Math.random() * 100);
     }
+
+    public void checkGuess(View )
 }
