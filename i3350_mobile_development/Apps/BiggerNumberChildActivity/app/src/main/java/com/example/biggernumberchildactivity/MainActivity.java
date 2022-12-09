bff3c7a1724e882cccf37cbd819bd18fcaa943d5
allawhussein <allawhussein@gmail.com>
2022-12-09 09:33:33 +0200

    added java code for bigger number android app

--- i3350_mobile_development/Apps/BiggerNumberChildActivity/app/src/main/java/com/example/biggernumberchildactivity/MainActivity.java
+++ i3350_mobile_development/Apps/BiggerNumberChildActivity/app/src/main/java/com/example/biggernumberchildactivity/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.biggernumberchildactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.*;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button leftBtn, rightBtn;
+    TextView points;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        leftBtn = findViewById(R.id.l_btn);
+        rightBtn = findViewById(R.id.r_btn);
+        points = findViewById(R.id.point_field);
+        points.setText("Points: 0/0");
+    }
+    private void generateNumber() {
+        int leftNumber = (int) (Math.random() * 100);
+        int rightNumber = (int) (Math.random() * 100);
+        while (leftNumber == rightNumber){
+            rightNumber = (int) (Math.random() * 100);
+        }
+
+        leftBtn.setText(""+leftNumber);
+        rightBtn.setText(""+rightNumber);
     }
 }
