99ea881717dbf635040959bac1db8547ed765bc3
allawhussein <allawhussein@gmail.com>
2023-01-04 12:46:17 +0200

    expanded on arithmatic challenge android app

--- i3350_mobile_development/Apps/ArithmaticChallenge/app/src/main/java/com/example/arithmaticchallenge/MainActivity.java
+++ i3350_mobile_development/Apps/ArithmaticChallenge/app/src/main/java/com/example/arithmaticchallenge/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private int leftNumber;
     private int rightNumber;
-    private char operation = ' ';
+    private String operation = "+";
     private int score;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         TextView rightNumberTextView = (TextView) findViewById(R.id.rightNumber);
         TextView operatorTextView= (TextView) findViewById(R.id.operator);
         TextView scoreTextView = (TextView) findViewById(R.id.scoreTextView);
-        EditText userInput = (EditText) findViewById(R.id.userAnswer);
+        EditText userAnswer = (EditText) findViewById(R.id.userAnswer);
 
         leftNumber = (int) (100 * Math.random());
         rightNumber = (int) (100 * Math.random());
@@ -35,10 +35,52 @@ public class MainActivity extends AppCompatActivity {
         rightNumberTextView.setText(String.valueOf(rightNumber));
         operatorTextView.setText(operation);
         scoreTextView.setText(String.format("Score: %d", score));
+        userAnswer.setText("");
     }
 
     public void checkAnswer(View submitButton){
+        EditText userInput = (EditText) findViewById(R.id.userAnswer);
+        int answer = Integer.parseInt(userInput.getText().toString());
+        if (operation == "+"){
+            if (answer == leftNumber + rightNumber) score++;
+            else score--;
+            populateTextViews();
+        }
+        else if (operation == "-"){
+            if (answer == leftNumber - rightNumber) score++;
+            else score--;
+            populateTextViews();
+        }
+        else if (operation == "*"){
+            if (answer == leftNumber * rightNumber) score++;
+            else score--;
+            populateTextViews();
+        }
+        else if (operation == "/"){
+            if (answer == leftNumber / rightNumber) score++;
+            else score--;
+            populateTextViews();
+            refreshNumbers(findViewById(R.id.divRadio));
+        }
 
     }
 
+    public void refreshNumbers(View view) {
+        TextView leftNumberTextView = (TextView) findViewById(R.id.leftNumber);
+        TextView rightNumberTextView = (TextView) findViewById(R.id.rightNumber);
+        TextView operatorTextView= (TextView) findViewById(R.id.operator);
+
+        rightNumber = (int) (10 * Math.random());
+        leftNumber = (int) (10 * Math.random()) * leftNumber;
+
+        leftNumberTextView.setText(String.valueOf(leftNumber));
+        rightNumberTextView.setText(String.valueOf(rightNumber));
+    }
+
+    public void refreshOperator(View view){
+        TextView operatorTextView= (TextView) findViewById(R.id.operator);
+        if (view.getId() == R.id.addRadio) operatorTextView.setText("+");
+        else if (view.getId() == R.id.subRadio) operatorTextView.setText("-");
+        else if (view.getId() == R.id.mulRadio) operatorTextView.setText("*");
+    }
 }
