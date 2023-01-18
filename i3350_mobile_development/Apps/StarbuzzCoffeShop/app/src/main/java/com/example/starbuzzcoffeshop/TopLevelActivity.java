f2552d62a0aebdc8802be0e1add13f52c03b39d2
allawhussein <allawhussein@gmail.com>
2023-01-18 07:35:01 +0200

    finished starbuzz android app welcome layout

---
+++ i3350_mobile_development/Apps/StarbuzzCoffeShop/app/src/main/java/com/example/starbuzzcoffeshop/TopLevelActivity.java
@@ -0,0 +1,14 @@
+package com.example.starbuzzcoffeshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TopLevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_level);
+    }
+}
